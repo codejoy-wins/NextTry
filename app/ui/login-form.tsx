94c1689ac28e378ea43f51a1c470279d240c420a
Maxwell Lawrence Jann <maxwellljann@gmail.com>
2024-05-20 13:05:03 -0500

    syn

--- app/ui/login-form.tsx
+++ app/ui/login-form.tsx
@@ -1,5 +1,5 @@
 'use client';
-
+ 
 import { lusitana } from '@/app/ui/fonts';
 import {
   AtSymbolIcon,
@@ -7,16 +7,15 @@ import {
   ExclamationCircleIcon,
 } from '@heroicons/react/24/outline';
 import { ArrowRightIcon } from '@heroicons/react/20/solid';
-import { Button } from './button';
+import { Button } from '@/app/ui/button';
 import { useFormState, useFormStatus } from 'react-dom';
 import { authenticate } from '@/app/lib/actions';
-
+ 
 export default function LoginForm() {
   const [errorMessage, dispatch] = useFormState(authenticate, undefined);
-
+ 
   return (
-        <form action={dispatch} className="space-y-3">
-
+    <form action={dispatch} className="space-y-3">
       <div className="flex-1 rounded-lg bg-gray-50 px-6 pb-4 pt-8">
         <h1 className={`${lusitana.className} mb-3 text-2xl`}>
           Please log in to continue.
@@ -63,18 +62,6 @@ export default function LoginForm() {
           </div>
         </div>
         <LoginButton />
-        <h1 className={`${lusitana.className} mb-3 text-2xl p-4`}>
-          Change for the Good
-        </h1>
-        <div className='red'>
-            <p>
-              You're welcome to use user@nextmail.com
-            </p>
-            <p>
-              Password is 123456
-            </p>
-        </div>
-
         <div
           className="flex h-8 items-end space-x-1"
           aria-live="polite"
@@ -91,10 +78,10 @@ export default function LoginForm() {
     </form>
   );
 }
-
+ 
 function LoginButton() {
   const { pending } = useFormStatus();
-
+ 
   return (
     <Button className="mt-4 w-full" aria-disabled={pending}>
       Log in <ArrowRightIcon className="ml-auto h-5 w-5 text-gray-50" />
